e3bbc3b382fe626c38f67feda954d25688610105
Phil Gonzalez <pgonzalez115@gmail.com>
2019-03-06 22:10:10 -0700

    Implement the preprocessor and use strcat

--- interactive_prompt.c
+++ interactive_prompt.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#ifdef _WIN32
+char os[] = "Windows";
+#else
+char os[] = "OSX";
+#endif
 
 #include<editline/readline.h>
 
 
 int main(int argc, char** argv) {
-  puts("Lispy Verison 0.1");
+  char version[100] = "Lispy Verison 0.1 ";
+  strcat(version, os);
+  puts(version);
   puts("Press Ctrl+c to Exit\n");
-
   while (1) {
     char* input = readline("lispy> ");
     add_history(input);
