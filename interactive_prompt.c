075604a2218524f5cf5097473171059bac6650ee
Phil Gonzalez <pgonzalez115@gmail.com>
2019-04-10 21:12:56 -0600

    Add fake readline for win machines

--- interactive_prompt.c
+++ interactive_prompt.c
@@ -4,18 +4,34 @@
 
 #ifdef _WIN32
 char os[] = "Windows";
+
+static char buffer[2048];
+
+/*Fake readline function*/
+char* readline(char* prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc((strlen(buffer) + 1));
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy) -1] = '\0';
+  return cpy;
+}
+
 #else
 char os[] = "OSX";
+#include<editline/readline.h>
 #endif
 
-#include<editline/readline.h>
+
 
 
 int main(int argc, char** argv) {
-  char version[100] = "Lispy Verison 0.1 ";
+  
+  char version[50] = "Lispy Verison 1.0 on ";
   strcat(version, os);
   puts(version);
   puts("Press Ctrl+c to Exit\n");
+  
   while (1) {
     char* input = readline("lispy> ");
     add_history(input);
