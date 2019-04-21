2bc452b2663886a6f65b8e8e7cfa81a61bf735b7
Phil Gonzalez <pgonzalez115@gmail.com>
2019-04-21 17:42:11 -0600

    Implement Polish Notation Parser

---
+++ parsing.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "mpc.h"
+
+#ifdef _WIN32
+char os[] = "Windows";
+
+static char buffer[2048];
+
+/*Fake readline function*/
+char* readline(char* prompt) {
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc((strlen(buffer) + 1));
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy) -1] = '\0';
+  return cpy;
+}
+
+#else
+char os[] = "OSX";
+#include<editline/readline.h>
+#endif
+
+
+
+
+int main(int argc, char** argv) {
+
+  mpc_parser_t* Number = mpc_new("number");
+  mpc_parser_t* Operator = mpc_new("operator");
+  mpc_parser_t* Expr = mpc_new("expr");
+  mpc_parser_t* Lispy = mpc_new("lispy");
+
+  mpca_lang(MPCA_LANG_DEFAULT,
+  "                                                    \
+  number   :  /-?[0-9]+/ ;                             \
+  operator :  '+' | '-' | '*' | '/';                   \
+  expr     :  <number> | '(' <operator> <expr>+ ')';   \
+  lispy    : /^/ <operator> <expr>+ /$/ ;              \
+  ",
+  Number, Operator, Expr, Lispy);
+  
+  char version[50] = "Lispy Verison 1.0 on ";
+  strcat(version, os);
+  puts(version);
+  puts("Press Ctrl+c to Exit\n");
+  
+  while (1) {
+    char* input = readline("lispy> ");
+    add_history(input);
+    /* Attempt to parse the user input */
+    mpc_result_t r;
+    if (mpc_parse("<stdin>", input, Lispy, &r)) {
+      /* On success, print AST */
+      mpc_ast_print(r.output);
+      mpc_ast_delete(r.output);
+    } else {
+      /* Otherwise, print the error */
+      mpc_err_print(r.error);
+      mpc_err_delete(r.error);
+    }
+    free(input);
+  }
+
+  mpc_cleanup(4, Number, Operator, Expr, Lispy);
+  return 0;
+};
